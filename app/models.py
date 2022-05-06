f99f99d7e1fb515c9990037a0f0bb4cf6aa2da4f
Yogesh Joshi <Yogesh.j@netsmartz.net>
2022-05-06 09:49:05 +0530

    update models

--- app/models.py
+++ app/models.py
@@ -6,3 +6,7 @@ class Item(Base):
 	__tablename__ = "items"
 
 	id = Column(Integer, primary_key=True, index=True)
+	name = Column(String(80), nullable=False, unique=True,index=True)
+	price = Column(Float(precision=2), nullable=False)
+	description = Column(String(200))
+	store_id = Column(Integer,ForeignKey('stores.id'),nullable=False)
